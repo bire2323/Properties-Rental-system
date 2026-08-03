a56adbd0a6ad82a618c8e8312ab15cf0f2724bb7
Birhanu Derso <birhanuderso10@gmail.com>
2026-08-03 21:30:48 +0300

    the secret key is placed in the .env file

--- django_Backend/config/settings.py
+++ django_Backend/config/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e=y8&f9zlzhcdpu2a&x@!!muvot*96450ve22ib+ckqv!l3q)x'
+SECRET_KEY =config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
