791c74984dc2aee3d6955bb21953a090fc90a9fb
Birhanu Derso <birhanuderso10@gmail.com>
2026-09-24 22:20:39 +0300

    Add gunicorn for Render deployment

--- django_Backend/config/settings.py
+++ django_Backend/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["cellulolytic-nonshredding-kena.ngrok-free.dev", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["cellulolytic-nonshredding-kena.ngrok-free.dev", "localhost", "127.0.0.1", "https://getspace-v1.vercel.app",]
 
 
 
