2b67067414a90f864f7564b448c2ffb9781b666b
Birhanu Derso <birhanuderso10@gmail.com>
2026-09-24 22:06:28 +0300

    Add gunicorn for Render deployment

--- django_Backend/config/settings.py
+++ django_Backend/config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 ALLOWED_HOSTS = ["cellulolytic-nonshredding-kena.ngrok-free.dev", "localhost", "127.0.0.1"]
 
